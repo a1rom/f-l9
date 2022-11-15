b4c1809bcaeeace2da027f5d1ea16e642cb1a482
alrom12 <dev@alrom.de>
2022-11-15 19:43:27 +0200

    breeze filament initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,18 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import path from 'path';
+import laravel, { refreshPaths } from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-        laravel([
-            'resources/js/app.js',
-        ]),
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: [
+                ...refreshPaths,
+                'app/Http/Livewire/**',
+                'app/Tables/Columns/**',
+            ], 
+        }),
     ],
-    resolve: {
-        alias: {
-            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-        }
-    },
-})
-
-// export default defineConfig({
-//     plugins: [
-//         laravel({
-//             input: ['resources/css/app.css', 'resources/js/app.js'],
-//             refresh: true,
-//         }),
-//     ],
-// });
+});
