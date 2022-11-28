365bc6969e32ff1d916651517a51a992ad906f1e
a1rom <dev@alrom.de>
2022-11-28 18:03:39 +0200

    purchase

---
+++ database/migrations/2022_11_22_122810_create_purchases_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->foreignId('supplier_id')->constrained();
+            $table->foreignId('product_id')->constrained();
+            $table->integer('quantity');
+            $table->decimal('price_vat_excl', 8, 2);
+            $table->decimal('total_vat', 8, 2);
+            $table->foreignId('vat_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchases');
+    }
+};
