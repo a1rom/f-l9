84b4db39a7b9711da26a4ee90334b2474beac822
a1rom <dev@alrom.de>
2022-12-01 13:02:18 +0200

    product categories crud completed

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,12 @@
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Models\ProductCategory;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MyController;
 use App\Http\Resources\ProductResourceJson;
 use App\Http\Middleware\Api\EnsureTokenIsValid;
-use App\Http\Controllers\Api\GetProductsController;
+use App\Http\Resources\ProductCategoryResourceJson;
+use App\Http\Controllers\Api\ProductCategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,12 +26,35 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::middleware(EnsureTokenIsValid::class)->group(function () {
     Route::prefix(config('my.api_version'))->group(function () {
-        Route::get('/products', function () {
-            return ProductResourceJson::collection(Product::paginate(config('my.api_paginate')));
+        Route::prefix('products')->group(function () {
+            Route::get('/', function () {
+                return ProductResourceJson::collection(Product::paginate(config('my.api_paginate')));
+            });
+
+            Route::get('/{id}', function ($id) {
+                return ProductResourceJson::make(Product::find($id));
+            });
         });
 
-        Route::get('/products/{id}', function ($id) {
-            return ProductResourceJson::make(Product::find($id));
+        Route::prefix('product-categories')->group(function () {
+            Route::get('/', function () {
+                return ProductCategoryResourceJson::collection(ProductCategory::paginate(config('my.api_paginate')));
+            });
+
+            Route::get('/{id}', function ($id) {
+                return ProductCategoryResourceJson::make(ProductCategory::find($id));
+            });
+
+            Route::post('/', [ProductCategoryController::class, 'store']);
+
+            Route::put('/{id}', [ProductCategoryController::class, 'update']);
+
+            Route::delete('/{id}', [ProductCategoryController::class, 'destroy']);
+
+            Route::fallback(function () {
+                return response()->json([
+                    'message' => 'Resource not found'], 404);
+            });
         });
     });
 });
