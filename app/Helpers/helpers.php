b6dc4f2d19e0cafa18b2567b2093ca653ac7ed90
a1rom <dev@alrom.de>
2022-12-01 13:20:21 +0200

    type message auto update

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -8,8 +8,21 @@ if (!function_exists('answerWithData')) {
      */
     function answerWithData(string $message, int $code): array
     {
+        if (floor($code/100) == 2) {
+            $type = 'success';
+        }
+        elseif (floor($code/100) == 4) {
+            $type = 'error';
+        }
+        elseif (floor($code/100) == 5) {
+            $type = 'error';
+        }
+        else {
+            $type = 'warning';
+        }
+
         return [
-            'type' => 'error',
+            'type' => $type,
             'attributes' => [
                 'code' => $code,
                 'message' => $message,
